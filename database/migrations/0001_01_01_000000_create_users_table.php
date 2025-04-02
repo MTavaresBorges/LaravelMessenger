94a2e33f2db40026a980546a45a668c14d44a593
MTavaresBorges <matheustavaresborges2000@gmail.com>
2025-04-02 19:38:41 -0300

    feat: migrations have been created

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('birthday');
+            $table->string('phone');
+            $table->string('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
